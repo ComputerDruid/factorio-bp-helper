dd169b386af7ebfa9815bf2c06fe755ed4716693
Dan Johnson <ComputerDruid@gmail.com>
2025-08-21 14:28:54 -0700

    rework name autogeneration to consistently use tags and support quality

--- src/save.rs
+++ src/save.rs
@@ -5,96 +5,152 @@ use std::{
     path::{Path, PathBuf},
 };
 
-pub fn save(mut json: serde_json::Value, dir: Option<&Path>) {
+fn format_tag(
+    typ: &str,
+    name: Option<&serde_json::Value>,
+    quality: Option<&serde_json::Value>,
+) -> Option<String> {
+    let name = name?.as_str()?;
+    let quality_suffix = if let Some(quality) = quality
+        && let Some(quality) = quality.as_str()
+        && quality != "normal"
+    {
+        format!(",quality={quality}")
+    } else {
+        String::new()
+    };
+    Some(format!("[{typ}={name}{quality_suffix}]"))
+}
+
+fn format_entity_tag(entity: &serde_json::Value) -> Option<String> {
+    format_tag("entity", entity.get("name"), entity.get("quality"))
+}
+
+fn format_icon_tag(icon: &serde_json::Value) -> Option<String> {
+    format_tag("icon", icon.get("name"), icon.get("quality"))
+}
+
+fn format_tile_tag(tile: &serde_json::Value) -> Option<String> {
+    format_tag("tile", tile.get("name"), tile.get("quality"))
+}
+
+fn unique_strings(iter: impl Iterator<Item = String>) -> impl Iterator<Item = String> {
+    let mut seen: std::collections::HashSet<String> = std::collections::HashSet::new();
+    iter.flat_map(move |s| {
+        if seen.contains(&s) {
+            None
+        } else {
+            seen.insert(s.clone());
+            Some(s)
+        }
+    })
+}
+
+fn compute_name(json: &serde_json::Value) -> String {
     let mut name = String::new();
     if let Some(thing) = json
         .get("blueprint")
         .or(json.get("blueprint_book"))
         .or(json.get("upgrade_planner"))
         .or(json.get("deconstruction_planner"))
-        && let Some(label) = thing.get("label")
-        && let Some(label) = label.as_str()
     {
-        name = label.to_owned();
-    } else if let Some(blueprint) = json.get("blueprint")
-        && let Some(icons) = blueprint.get("icons")
-        && let Some(icons) = icons.as_array()
-    {
-        for icon in icons {
-            if let Some(signal) = icon.get("signal")
-                && let Some(signal_name) = signal.get("name")
-                && let Some(signal_name) = signal_name.as_str()
-            {
-                if !name.is_empty() {
-                    name.push(' ');
-                }
-                name.push_str(signal_name);
-            }
-        }
-    } else if let Some(blueprint) = json.get("deconstruction_planner")
-        && let Some(settings) = blueprint.get("settings")
-    {
-        let entities = settings.get("entity_filters");
-        let entities = entities
-            .iter()
-            .flat_map(|e| e.as_array())
-            .flatten()
-            .flat_map(|e| e.get("name"))
-            .flat_map(|e| e.as_str())
-            .map(|entity_name| format!("[entity={entity_name}]"));
-
-        let tiles = settings.get("tile_filters");
-        let tiles = tiles
-            .iter()
-            .flat_map(|e| e.as_array())
-            .flatten()
-            .flat_map(|e| e.get("name"))
-            .flat_map(|e| e.as_str())
-            .map(|tile_name| format!("[tile={tile_name}]"));
-
-        let mut iter = entities.chain(tiles).fuse();
-
-        for name_part in (&mut iter).take(4) {
-            if !name.is_empty() {
-                name.push(' ');
-            }
-            name.push_str(&name_part);
-        }
-        if iter.next().is_some() {
-            name.push_str(" …");
-        }
-    } else if let Some(blueprint) = json.get("upgrade_planner")
-        && let Some(settings) = blueprint.get("settings")
-        && let Some(mappers) = settings.get("mappers")
-        && let Some(mappers) = mappers.as_array()
-    {
-        let mut iter = mappers.iter().fuse();
-        for mapper in (&mut iter).take(4) {
-            if let Some(to) = mapper.get("to")
-                && let Some(name_part) = to.get("name")
-                && let Some(name_part) = name_part.as_str()
-            {
-                if !name.is_empty() {
-                    name.push(' ');
-                }
-                if let Some(typ) = to.get("type")
-                    && let Some(typ) = typ.as_str()
-                    && typ == "entity"
+        if let Some(label) = thing.get("label")
+            && let Some(label) = label.as_str()
+        {
+            name = label.to_owned();
+        } else if let Some(icons) = thing.get("icons").or(thing
+            .get("settings")
+            .and_then(|settings| settings.get("icons")))
+            && let Some(icons) = icons.as_array()
+        {
+            for icon in icons {
+                if let Some(signal) = icon.get("signal")
+                    && let Some(signal_name) = format_icon_tag(signal)
                 {
-                    name.push_str(&format!("[entity={name_part}]"));
-                } else {
-                    name.push_str(&name_part);
+                    if !name.is_empty() {
+                        name.push(' ');
+                    }
+                    name.push_str(&signal_name);
                 }
             }
-        }
-        if !name.is_empty() {
-            name = format!("Upgrade {name}");
+        } else if let Some(blueprint) = json.get("deconstruction_planner")
+            && let Some(settings) = blueprint.get("settings")
+        {
+            let entities = settings.get("entity_filters");
+            let entities = entities
+                .iter()
+                .flat_map(|e| e.as_array())
+                .flatten()
+                .flat_map(format_entity_tag);
+
+            let tiles = settings.get("tile_filters");
+            let tiles = tiles
+                .iter()
+                .flat_map(|e| e.as_array())
+                .flatten()
+                .flat_map(format_tile_tag);
+
+            let mut iter = entities.chain(tiles).fuse();
+
+            for name_part in (&mut iter).take(4) {
+                if !name.is_empty() {
+                    name.push(' ');
+                }
+                name.push_str(&name_part);
+            }
+            if iter.next().is_some() {
+                name.push_str("…");
+            }
+        } else if let Some(blueprint) = json.get("upgrade_planner")
+            && let Some(settings) = blueprint.get("settings")
+            && let Some(mappers) = settings.get("mappers")
+            && let Some(mappers) = mappers.as_array()
+        {
+            let mut iter = unique_strings(
+                mappers
+                    .iter()
+                    .flat_map(|mapper| mapper.get("to"))
+                    .flat_map(|to| {
+                        let typ = to.get("type");
+                        if let Some(typ) = typ
+                            && let Some(typ) = typ.as_str()
+                            && let Some(name_part) =
+                                format_tag(typ, to.get("name"), to.get("quality"))
+                        {
+                            Some(name_part)
+                        } else if let Some(name_part) = to.get("name")
+                            && let Some(name_part) = name_part.as_str()
+                        {
+                            Some(name_part.to_owned())
+                        } else {
+                            None
+                        }
+                    }),
+            )
+            .fuse();
+            for name_part in (&mut iter).take(4) {
+                if !name.is_empty() {
+                    name.push(' ');
+                }
+                name.push_str(&name_part);
+            }
+            if iter.next().is_some() {
+                name.push_str("…");
+            }
+            if !name.is_empty() {
+                name = format!("Upgrade {name}");
+            }
         }
     }
 
     if name.is_empty() {
         name = "Untitled".to_owned()
     };
+    name
+}
+
+pub fn save(mut json: serde_json::Value, dir: Option<&Path>) {
+    let mut name = compute_name(&json);
 
     if let Some(index) = json.get_mut("index")
         && let Some(index) = index.as_number()
@@ -173,9 +229,9 @@ mod tests {
         let json = serde_json::Value::from_str(&json).expect("should contain valid json");
         save(json.clone(), Some(dir.path()));
         let files = read_dir_unwrap(dir.path());
-        assert_eq!(files, &["selector-combinator.json"]);
+        assert_eq!(files, &["[icon=selector-combinator].json"]);
         let written_json =
-            std::fs::read_to_string(dir.path().join("selector-combinator.json")).unwrap();
+            std::fs::read_to_string(dir.path().join("[icon=selector-combinator].json")).unwrap();
         let written_json = serde_json::Value::from_str(&written_json).unwrap();
         assert_eq!(written_json, json);
     }
@@ -192,7 +248,7 @@ mod tests {
         let subfiles = read_dir_unwrap(&dir.path().join("Untitled"));
         assert_eq!(subfiles, ["0 BP Name 1.json", "1 Nested Book", "book.json"]);
         let subsubfiles = read_dir_unwrap(&dir.path().join("Untitled/1 Nested Book"));
-        assert_eq!(subsubfiles, ["6 bulk-inserter.json", "book.json"]);
+        assert_eq!(subsubfiles, ["6 [icon=bulk-inserter].json", "book.json"]);
 
         // TODO: when load is implemented, test that it can load everything back properly.
     }
@@ -228,5 +284,32 @@ mod tests {
         let written_json = std::fs::read_to_string(dir.path().join(EXPECTED_NAME)).unwrap();
         let written_json = serde_json::Value::from_str(&written_json).unwrap();
         assert_eq!(written_json, json);
+    }
+
+    #[test]
+    fn test_dedupe_names_upgrade_planner() {
+        let bp = "0eNq1UMsKwjAQ/Jc9V5A+LA34JSIS2rUGmt2YbNVS8u8mYK968jaPZXaYFWY3ej3gxU2aCD2oFQKKGBpDxlY7hz7B0wpXzzZrsjgEBUhiZIECSNvMn8wD0q6/YZCk3mc9ZV8Bsbd6SlLP1mmvhdMbOEIsQPhLYBDE6ZOXbg0N+AK1j8XPKsbz/4vU8ZyJYGqyzbjbZizgkWYzTKCaQ9nVXde0VVPVbRnjGw8nfKM=";
+        let json = crate::blueprint::blueprint_to_json(bp);
+        let json = serde_json::Value::from_str(&json).expect("should contain valid json");
+        assert_eq!(compute_name(&json), "Upgrade [entity=steel-chest]")
+    }
+
+    #[test]
+    fn test_upgrade_remove_module() {
+        let bp = "0eNp1js0KwjAQhN9lzy1IfywN+CQiEsxaAtlkTbZiKXl3E7BHbzPfDrOzw8pL1Abv7LT3GEHtkFDE+iVVTZoZY5HXHZ4xUGWyMYICK0jQgNdUXWJE01Iwq8NCX6t2VrZy8CGSdgU9ArGOWkJ5AhfIDUj4W4fEsv3q2uSC1Lz1Bj+gTvlWTc2rY3977G/gXfba4EGN524e5nmc+rEfpi7nL1TiT8I=";
+        let json = crate::blueprint::blueprint_to_json(bp);
+        let json = serde_json::Value::from_str(&json).expect("should contain valid json");
+        assert_eq!(compute_name(&json), "Upgrade [item=empty-module-slot]")
+    }
+
+    #[test]
+    fn test_upgrade_quality() {
+        let bp = "0eNqVT1sKwjAQvMt+V5A+LA14EhHZtmsNdDcx3Yql5O4miAfwbx7M7M4Oq58CjnTzM4pQALPDQqpWpiVjRu8pJHjZ4R4cZ003T2CARK1uUIAgZ95bNzyQ+1RSwHPFObsGxAXGOUmDY48B1aUjcIZYgLr/61ZJPewk562M9AZzjNdMlDj73z2H354CXul/mwKmOZVd3XVNWzVV3ZYxfgDMH1WE";
+        let json = crate::blueprint::blueprint_to_json(bp);
+        let json = serde_json::Value::from_str(&json).expect("should contain valid json");
+        assert_eq!(
+            compute_name(&json),
+            "Upgrade [entity=biochamber,quality=uncommon]"
+        )
     }
 }
